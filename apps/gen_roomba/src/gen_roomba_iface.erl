9de7fdb1635eacf2a986f3f5ab18552285c6cdf6
Sebastian Green-Husted <ricecake@tfm.nu>
2015-08-24 15:04:10 -0400

    add drive function, and some utilities

--- apps/gen_roomba/src/gen_roomba_iface.erl
+++ apps/gen_roomba/src/gen_roomba_iface.erl
@@ -2,6 +2,8 @@
 
 -export([
 	open/1, close/1,
+	passive/1, active/1,
+	drive/3,
 	send_bytes/2,
 	read_bytes/2
 ]).
@@ -24,6 +26,18 @@ open(Device) when is_list(Device) ->
 close(FD) ->
 	ok = serctl:write(FD, <<128:8, 133:8>>),
 	serctl:close(FD).
+
+passive(FD) -> gen_roomba_iface:send_bytes(FD, <<128:8>>).
+active(FD)  -> gen_roomba_iface:send_bytes(FD, <<131:8>>).
+
+drive(FD, 0,     right) -> drive(FD, 0, -1);
+drive(FD, 0,     left)  -> drive(FD, 0,  1);
+drive(FD, Speed, right) -> drive(FD, Speed, -2000);
+drive(FD, Speed, left)  -> drive(FD, Speed,  2000);
+drive(FD, Speed, none)  -> drive(FD, Speed, 32768);
+drive(FD, Speed, Radius) when (Speed >= -500) and (Speed =< 500) and
+			      (Radius >= -32768) and (Radius =< 32768) ->
+	gen_roomba_iface:send_bytes(FD, <<137, Speed:16, Radius:16>>).
 
 send_bytes(FD, Bytes) -> serctl:write(FD, Bytes).
 read_bytes(_FD, _Bytes) -> ok.
