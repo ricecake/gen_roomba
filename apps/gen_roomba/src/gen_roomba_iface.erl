e0da9e23c5928a1e9aef645b9837b347e19f4a1e
Sebastian Green-Husted <ricecake@tfm.nu>
2015-08-24 13:48:27 -0400

    get the basics of open working

--- apps/gen_roomba/src/gen_roomba_iface.erl
+++ apps/gen_roomba/src/gen_roomba_iface.erl
@@ -1,9 +1,29 @@
 -module(gen_roomba_iface).
 
 -export([
+	open/1, close/1,
 	send_bytes/1,
 	read_bytes/1
 ]).
 
-send_bytes(Bytes) -> ok.
-read_bytes(Bytes) -> ok.
+open(Device) when is_list(Device) ->
+	{ok, FD} = serctl:open(Device),
+	Termios = lists:foldl(
+	    fun(Fun, Acc) -> Fun(Acc) end,
+	    serctl:mode(raw),
+	    [
+		fun(N) -> serctl:flow(N, false) end,
+		fun(N) -> serctl:ispeed(N, b115200) end,
+		fun(N) -> serctl:ospeed(N, b115200) end
+	    ]
+	),
+	ok = serctl:tcsetattr(FD, tcsanow, Termios),
+	ok = serctl:write(FD, <<128:8, 131:8>>),
+	{ok, FD}.
+
+close(FD) ->
+	ok = serctl:write(FD, <<128:8>>),
+	serctl:close(FD).
+
+send_bytes(FD, Bytes) -> serctl:write(FD, Bytes).
+read_bytes(FD, Bytes) -> ok.
